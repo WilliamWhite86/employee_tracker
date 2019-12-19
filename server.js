5cb079f0782f8bcdf252fa2e59c3b649b9963051
William White <william.white86@gmail.com>
2019-12-18 23:48:53 -0500

    created first 2 queries for app

--- server.js
+++ server.js
@@ -10,11 +10,10 @@ const connection = mysql.createConnection({
 
 connection.connect();
 
-connection.query('SELECT * FROM department', function (error, results){
-    if (error) throw error;
-    console.log(results)})
-
-connection.end()
+connection.query('SELECT * FROM department', function (error, results) {
+    if (error) throw error
+    console.table(results)
+})
 
 function firstPrompt() {
     return inquirer.prompt([
@@ -22,15 +21,22 @@ function firstPrompt() {
             type: "list",
             name: "toDo",
             message: "What what you like to do?",
-            choices: ["view all employees by department", "view all employees by role", "add employee", "remove employee", "update employee role", "view all roles"]
+            choices: ["view all employees", "view all employees by department", "view all employees by role", "add employee", "remove employee", "update employee role", "view all roles"]
         }
     ])
 }
 
 firstPrompt().then(function (value) {
     switch (value.toDo) {
+        case "view all employees":
+            allEmployeesQuery()
+            connection.end()
+            break;
         case "view all employees by department":
-            console.log("query to view all employees by dept")
+            PromptAllEmployeesbyDepartment().then(function (value) {
+                allEmployeesbyDeptQuery(value)
+                connection.end()
+            })
             break;
         case "view all employees by role":
             console.log("query to view all employees by role")
@@ -49,4 +55,30 @@ firstPrompt().then(function (value) {
     }
 
 })
+
+function allEmployeesQuery() {
+    connection.query('SELECT * FROM employee', function (error, results) {
+        if (error) throw error
+        console.table(results)
+    })
+}
+
+function PromptAllEmployeesbyDepartment() {
+    return inquirer.prompt([
+        {
+            type: "list",
+            name: "alldepartments",
+            choices: ["Legal", "Sales", "Finance", "IT"]
+        }
+    ])
+}
+
+function allEmployeesbyDeptQuery(value) {
+    connection.query(`SELECT employee.first_name, employee.last_name, department.name FROM employee INNER JOIN role on employee.role_id = role.id INNER JOIN department on department.id = role.department_id WHERE department.name = '${value.alldepartments}';`, function (error, results) {
+        if (error) throw error
+        console.table(results)
+    })
+}
+
+
 
